d72e77213ae9400f75e95b2fd10b2ad8c4e6818b
Scott Martin <scooter@ianscottmartin@gmail.com>
2020-06-18 11:48:56 -0600

    another

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const parentElement = document.querySelector('.katas-list')
+
+//kata1
+const kata1Heading = document.createElement('h3')
+parentElement.append(kata1Heading)
+kata1Heading.append('Kata 1')
+
+// solve kata1
+//append 
+
+//kata2
